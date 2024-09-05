1df25042b589f711cd3d8df57a03153fb7f8e5c0
Katerina-cyber <kaatesemenchuk@gmail.com>
2024-09-05 18:16:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// Выбираем все ссылки, которые ведут на якорь
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        // Получаем элемент, на который ссылается href
+        const targetElement = document.querySelector(this.getAttribute('href'));
+
+        if (targetElement) {
+            // Плавно прокручиваем к цели
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
     });
 });
